7d809f08c4b2b89fda34ea41dc3b7cef739fb611
khushaboosonawane <sokhushaboo202@gmail.com>
2024-07-13 09:18:24 +0530

    All setup done

---
+++ application/controllers/Admincontroller.php
@@ -0,0 +1,9 @@
+<?php
+defined("BASEPATH") or exit("No direct script access is allowed");
+
+class Admincontroller extends CI_Controller{
+    public function index(){
+        echo "welcome in admin controller";
+    }
+}
+?>
---
+++ application/controllers/Usercontroller.php
@@ -0,0 +1,10 @@
+<?php
+defined("BASEPATH") or exit("No direct script access allowed");
+
+Class Usercontroller extends CI_Controller{
+    public function index(){
+        echo "Welcome in user controller";
+    }
+    
+}
+?>
